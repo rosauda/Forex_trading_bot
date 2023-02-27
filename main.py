eb092985d8f49b963691cd042e64a232dc470d7d
rodrigo.sauda <rodrigo.sauda@hotmail.com>
2023-02-27 12:07:05 +0100

    creating new directories & infrastructure

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from infrastructure.instrument_collection import instrumentCollection
 if __name__ == "__main__":
     api = OandaApi()
 
-    # data = api.get_account_summary()
-    # print(data)
-
+    instrumentCollection.CreateFile(api.get_account_instruments_(), "./data")
     instrumentCollection.LoadInstruments("./data")
     instrumentCollection.PrintInstruments()

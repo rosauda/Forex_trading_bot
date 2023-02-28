55651b4d318ea4cc7dedc2a535787ffd40edeeac
rodrigo.sauda <rodrigo.sauda@hotmail.com>
2023-02-28 17:15:38 +0100

    creating charts in Excel file

--- api/oanda_api.py
+++ api/oanda_api.py
@@ -1,5 +1,8 @@
 import requests
 import os
+import pandas as pd
+from dateutil import parser
+from datetime import datetime as dt
 
 API_KEY = os.environ["api_key"]
 ACCOUNT_ID = os.environ["account_id"]
@@ -53,3 +56,67 @@ class OandaApi:
 
     def get_account_instruments(self):
         return self.get_account_ep("instruments", "instruments")
+
+    def fetch_candles(self, pair_name, count=10, granularity="H1", price='MBA', date_f=None, date_t=None):
+        url = f"instruments/{pair_name}/candles"
+        params = dict(
+            granularity=granularity,
+            price=price,
+        )
+
+        if date_f is not None and date_t is not None:
+            date_format = "%Y-%m-%dT%H:%M:%SZ"
+            params["from"] = dt.strftime(date_f, date_format)
+            params["to"] = dt.strftime(date_t, date_format)
+        else:
+            params["count"] = count
+
+        ok, data = self.make_request(url, params=params)
+        if ok == True and 'candles' in data:
+            return data['candles']
+        else:
+            print("ERROR fetch_candles()", params, data)
+            return None
+
+    def get_candles_df(self, pair_name, **kwargs):
+
+        data = self.fetch_candles(pair_name, **kwargs)
+
+        if data is None or len(data) == 0:
+            return pd.DataFrame()
+
+        # Creating data frame
+        prices = ['mid', 'bid', 'ask']
+        ohlc = ['o', 'h', 'l', 'c']
+
+        final_data = [{**{'time': parser.parse(candle['time']), 'volume': candle['volume']},
+                       **{f"{p}_{o}": float(candle[p][o]) for p in prices if p in candle for o in ohlc}}
+                      for candle in data if candle['complete']]
+        df = pd.DataFrame.from_records(final_data)
+
+        return df
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from api.oanda_api import OandaApi
 from infrastructure.instrument_collection import instrumentCollection
 from simulation.ma_cross import run_ma_sim
+from dateutil import parser
+
+
 
 if __name__ == "__main__":
     api = OandaApi()
+    dfr = parser.parse("2021-04-21T01:00:00Z")
+    dto = parser.parse("2021-04-28T16:00:00Z")
 
-    instrumentCollection.CreateFile(api.get_account_instruments(), "./data")
-    instrumentCollection.LoadInstruments("./data")
-    instrumentCollection.PrintInstruments()
-    run_ma_sim(["EUR", "USD", "GBP", "JPY", "AUD", "CAD"])
+    df_candles = api.get_candles_df("EUR_USD", granularity="H1", date_f=dfr, date_t=dto)
+
+    print(df_candles.head())
+    print()
+    print(df_candles.tail())
+
+
+
+    # print(api.fetch_candles('EUR_USD', granularity="D", price="MB"))
+
+    # instrumentCollection.CreateFile(api.get_account_instruments(), "./data")
+    # instrumentCollection.LoadInstruments("./data")
+    # instrumentCollection.PrintInstruments()
+    # run_ma_sim(["EUR", "USD", "GBP", "JPY", "AUD", "CAD"])
